d6be02258274c20ed6d548caa60b0ee9c7a4a549
nessosnarmi <mdimran763272@gmail.com>
2023-01-01 13:30:49 +0600

    institude1/1/23

--- institute.php
+++ institute.php
@@ -1,8 +1,188 @@
 <!-- header; -->
 <?php include 'include/header.php' ; ?>
 
+<!DOCTYPE html>
+<html lang="en">
 
-<br>
+  <head>
+
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+
+    <title>Liberty NFT Marketplace - HTML CSS Template</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-liberty-market.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet"href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+   <!--
+
+TemplateMo 577 Liberty Market
+
+https://templatemo.com/tm-577-liberty-market
+
+-->
+  </head>
+
+  <body>
+  <!-- ***** Preloader Start ***** -->
+  <!--
+  <div id="js-preloader" class="js-preloader">
+    <div class="preloader-inner">
+      <span class="dot"></span>
+      <div class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  </div> -->
+  <!-- ***** Preloader End ***** -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <br><br><br>
+<footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          &nbsp;&nbsp;
+          <!--See detels... <a title="HTML CSS Templates" rel="sponsored" href="#" target="_blank">@pcoweb</a></p>-->
+                Who Participated our <i>Pritilota Caltual Org</i>?
+        </div>
+      </div>
+    </div>
+  </footer>
+  <br><br><br>
+
+<div class="col-lg-12">
+          <div class="row grid">
+            <div class="col-lg-6 currently-market-item all msc">
+              <div class="item">
+                <div class="left-image">
+                  <img src="images/IAGWC.jpg" alt="" style="border-radius: 20px; min-width: 195px;">
+                </div>
+                <div class="right-content">
+                <button>See detels... <a title="HTML CSS Templates" rel="sponsored" 
+href="http://www.iagwcollege.edu.bd/" target="_blank">Imran Ahamed Govt. Women's College</a></p></button>
+
+                <span class="author">
+                    <img src="assets/images/author.jpg" alt="" style="max-width: 50px; border-radius: 50%;">
+                    
+                  </div>
+              </div>
+            </div>
+                  
+
+                    <div class="col-lg-6 currently-market-item all vtr">
+              <div class="item">
+                <div class="left-image">
+                  <img src="images/GGC.jpg" alt="" style="border-radius: 20px; min-width: 195px;">
+                </div>
+                <div class="right-content">
+                  <button>See detels... <a title="HTML CSS Templates" rel="sponsored"
+                   href="https://www.ggcc.edu.bd/" target="_blank">Gasbariya govt. College</a></p></button>
+                    <span class="author">
+                    <img src="assets/images/author.jpg" alt="" style="max-width: 50px; border-radius: 50%;">
+
+          
+                   
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-6 currently-market-item all blc">
+              <div class="item">
+                <div class="left-image">
+                
+                  <img src="images/BGHS.jpg" alt="" style="border-radius: 20px; min-width: 195px;">
+                  <button>See detels... <a title="HTML CSS Templates" rel="sponsored" 
+href="https://bghs.tsmts.org/" target="_blank">Bakulia govt. College</a></p></button>
+
+                </div>
+                <div class="right-content">
+                    <span class="author">
+                    <img src="assets/images/author.jpg" alt="" style="max-width: 50px; border-radius: 50%;">
+                    
+                                  
+                </div>
+              </div>
+            </div>
+                  
+
+                    <div class="col-lg-6 currently-market-item all vtr">
+              <div class="item">
+                <div class="left-image">
+                  <img src="images/GMHS.jpg" alt="" style="border-radius: 20px; min-width: 195px;">
+                </div>
+                <div class="right-content">
+<button>See detels... <a title="HTML CSS Templates" rel="sponsored" 
+href="https://gmhsctg.tsmts.com/" target="_blank">Govt. Muslim High School</a></p></button>
+                    <span class="author">
+                    <img src="assets/images/author.jpg" alt="" style="max-width: 50px; border-radius: 50%;">
+                    
+                </div>
+              </div>
+            </div>
+            
+            <div class="col-lg-6 currently-market-item all blc">
+              <div class="item">
+                <div class="left-image">
+                
+                  <img src="images/PGC.jpg" alt="" style="border-radius: 20px; min-width: 195px;">
+                  <button>See detels... <a title="HTML CSS Templates" rel="sponsored"
+                   href="https://patiyagovtcollege.gov.bd/" target="_blank">Patiya Govt. High School</a></p></button>
+                </div>
+                <div class="right-content">
+                    <span class="author">
+                    <img src="assets/images/author.jpg" alt="" style="max-width: 50px; border-radius: 50%;">
+                  
+                                  
+                </div>
+              </div>
+            </div>
+
+           
+            <div class="col-lg-6 currently-market-item all vtr">
+              <div class="item">
+                <div class="left-image">
+                  <img src="images/KGSH.jpg" alt="" style="border-radius: 20px; min-width: 195px;">
+                </div>
+                <div class="right-content">
+<button>See detels... <a title="HTML CSS Templates" rel="sponsored"
+ href="https://kpscedu.org/" target="_blank">Kornopuli Govt. High School</a></p></button>
+ <span class="author">
+                    <img src="assets/images/author.jpg" alt="" style="max-width: 50px; border-radius: 50%;">
+                    
+                </div>
+              </div>
+            </div>
+
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <br><br><br>
+ </body>
+</html>
 
 <!-- footer -->
 <?php include 'include/footer.php' ; ?>
